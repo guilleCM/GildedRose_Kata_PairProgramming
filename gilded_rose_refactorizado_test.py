e2c21d1ab519234c06eb5ef64f86e3218a31a35d
Marc Hernandez <noviluni@gmail.com>
2016-12-19 19:24:27 +0100

    Add AgedBrie Class and test

--- gilded_rose_refactorizado_test.py
+++ gilded_rose_refactorizado_test.py
@@ -50,3 +50,12 @@ class Sulfuras(NormalItem):
     def update_quality(self):
         assert self.quality == 80, "Quality de %s no es 80" % self.__class__.__name__
         pass
+
+class AgedBrie(NormalItem):
+
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality) 
+
+    def update_quality(self):
+        self.setQuality(1)        
+        self.setSell_in()
